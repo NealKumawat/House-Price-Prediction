a8f593a5e88fa1144a47b41d4d37744bbefe0760
NealKumawat <nealkumawat04@gmail.com>
2025-06-16 21:56:39 +0000

    updated

--- Prediction/code.py
+++ Prediction/code.py
@@ -6,17 +6,10 @@ from sklearn import datasets
 
 
 # Define the relative path to the CSV file
-csv_file = 'data/my_data.csv'
+csv_file = 'Prediction/Data/Bengaluru_House_Data.csv'
 
 # Load the CSV file
 df = pd.read_csv(csv_file)
 
-# Print basic info
-print("Column Names:")
-print(df.columns.tolist())
-
-print("\nData Types:")
-print(df.dtypes)
-
-print("\nShape of the dataset:")
+df = df.drop(['availability', 'society', 'bath'], axis=1)
 print(df.shape)
