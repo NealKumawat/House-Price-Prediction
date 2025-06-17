0892ac41d8a3aae7b943ba6b7c65de6aa1416c54
NealKumawat <nealkumawat04@gmail.com>
2025-06-17 20:24:06 +0000

    updated

--- Prediction/justtoremember.py
+++ Prediction/justtoremember.py
@@ -1,1 +1,8 @@
 df['column'] = df['column'].str.strip("123./@#$%^&*")
+this would remove 1,2,3,.,/,@,#,$,%,^,&,* from the entire column
+
+
+if we have a column in which we want just numbers and not anything else so we follow the following script
+df['phone'] = df['phone'].str.replace('[^a-zA-Z0-9]', '')
+
+
